a5b55fc2c4bd7a1b80f8c89a67f9835f963ea9bf
Lucas Magalhaes <lucasfredmagalhaes@gmail.com>
2025-01-19 12:43:18 -0500

    Simplified golem movement

--- Scripts/golem.gd
+++ Scripts/golem.gd
@@ -1,70 +1,31 @@
 extends CharacterBody2D
 
-@export var speed = 150
+@export var walk_speed = 150
 
-var current_dir = "none"
- 
-var tiles = []
+@onready var animation : AnimatedSprite2D = $AnimatedSprite2D
+var desired_direction : Vector2 = Vector2.ZERO
 
 func _ready():
-	$Sprite2D.play("g_idle")
-func player():
-	pass
+	animation.play("g_idle")
 
 func get_input():
-	var input_direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
 	
-	if Input.is_action_pressed("ui_right"):
-		current_dir = "right"
-		play_anim(1)
-		velocity = input_direction * speed
-	elif Input.is_action_pressed("ui_left"):
-		current_dir = "left"
-		play_anim(1)
-		velocity = input_direction * speed
-	elif Input.is_action_pressed("ui_down"):
-		current_dir = "down"
-		play_anim(1)
-		velocity = input_direction * speed
-	elif Input.is_action_pressed("ui_up"):
-		current_dir = "up"
-		play_anim(1)
-		velocity = input_direction * speed
+	if input_direction == Vector2.ZERO: # not moving
+		animation.play("g_idle")
 	else:
-		play_anim(0)
-		velocity.x = 0
-		velocity.y = 0
+		if input_direction.x > 0: # moving right
+			animation.play("g_walk_side")
+		elif input_direction.x < 0: # moving left
+			animation.play("g_walk_side")
+		else:
+			if input_direction.y > 0: # moving straight up
+				animation.play("g_walk_up")
+			elif input_direction.y < 0: # moving straight down
+				animation.play("g_walk_down")
 	
+	velocity = input_direction * walk_speed
+
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
-
-func play_anim(movement):
-	var anim = $Sprite2D
-	var dir = current_dir
-
-	if dir == "right":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("g_walk_side")
-		elif movement == 0:
-			anim.play("g_idle")
-	if dir == "left":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("g_walk_side")
-		elif movement == 0:
-			anim.play("g_idle")
-
-	if dir == "down":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("g_walk_up")
-		elif movement == 0:
-			anim.play("g_idle")
-	if dir == "up":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("g_walk_down")
-		elif movement == 0:
-			anim.play("g_idle")
