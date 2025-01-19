54eace91eb75795bf89b81a8fc1399ffa9d4b412
MrHooga <aidensterling365@gmail.com>
2025-01-19 11:20:54 -0500

    Laser go brrrr
    it is assigned to "ui_select" for now

--- Scripts/base_animal.gd
+++ Scripts/base_animal.gd
@@ -4,5 +4,8 @@ func _physics_process(delta):
 	$AnimatedSprite2D.play("f_idle")
 	move_and_slide()
 
-func _on_area_2d_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
-	print("AHHHHH, IT HURTS, PLEASE STOOOOOP!!!!!")
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("die"):
+		print("AHHHHH, IT HURTS, PLEASE STOOOOOP!!!!!")
+		queue_free()
+	
