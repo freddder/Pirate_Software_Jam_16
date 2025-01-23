c5bcc09680ad5ffcaf59c7f9422cc5acfa113d5f
MrHooga <aidensterling365@gmail.com>
2025-01-22 23:30:37 -0500

    Tree do be treeing

--- Scenes/gold_tree.gd
+++ Scenes/gold_tree.gd
@@ -1,12 +1,12 @@
-extends CharacterBody2D
+extends Area2D
 class_name GoldenTree
 
+@onready var tree : Area2D = $"."
 @onready var tree_animation : AnimatedSprite2D = $AnimatedSprite2D
 var timer : float = 0.0
 
 func _ready():
 	Level.golden_trees.push_back(self)
-
 func get_hit(source : Vector2) -> bool:
 	tree_animation.play("tree_chopped")
 	return false
