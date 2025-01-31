757c3f7fb616ac9f94ef84a663d23d0b52f9a042
Lucas Magalhaes <lucasfredmagalhaes@gmail.com>
2025-01-30 23:15:06 -0500

    Added grab bubble

--- Golem/golem_grab.gd
+++ Golem/golem_grab.gd
@@ -3,6 +3,7 @@ class_name GolemGrab
 
 @onready var golem : CharacterBody2D = $"../.."
 @onready var grab_hitbox : Area2D = $"../../Grab/Hitbox"
+@onready var bubble : AnimatedSprite2D = $"../../Grab/Bubble"
 @export var grab_hitbox_speed : float = 400.0
 @export var grab_max_range : float = 200.0
 @export var grab_follow_speed : float = 200.0
@@ -17,9 +18,11 @@ func enter():
 		grab_hitbox.set_deferred("monitoring", true)
 		grab_hitbox.position = Vector2.ZERO
 		is_attempting_grab = true
+		bubble.visible = true
 		grab_hitbox_direction = (golem.get_global_mouse_position() - golem.global_position).normalized()
 	else:
 		is_grabbing = false
+		bubble.visible = false
 
 func exit():
 	golem.grab_cooldown = 0.5
@@ -37,18 +40,20 @@ func _process(delta):
 		var move_direction = (body_global_destination - grabbed_body.global_position)
 		if move_direction.length() > 10:
 			grabbed_body.global_position += move_direction.normalized() * grab_follow_speed * delta
+			bubble.global_position = grabbed_body.global_position
 
 func update(delta : float):
-	if Level.scene != "map":
-		grabbed_body.release()
 	if is_attempting_grab:
 		# Move grab hitbox until it finds something grabbable or it reaches max range
 		grab_hitbox.global_position += grab_hitbox_direction * grab_hitbox_speed * delta
+		bubble.global_position = grab_hitbox.global_position
 		if (grab_hitbox.global_position - golem.global_position).length() >= grab_max_range:
 			ChangeState.emit(self, "free")
+			bubble.visible = false
 	elif !is_grabbing:
 		grabbed_body.release()
 		grabbed_body = null
+		bubble.visible = false
 		ChangeState.emit(self, "free")
 
 func physic_update(delta : float):
