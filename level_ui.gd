a84b79918b04a84acd1f10fe97d0e4f537a323ae
MrHooga <aidensterling365@gmail.com>
2025-01-31 03:39:16 -0500

    da bar be barin

--- level_ui.gd
+++ level_ui.gd
@@ -5,7 +5,7 @@ class_name LevelUI
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	integrity_bar.value = integrity_bar.max_value
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
